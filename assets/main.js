0c93ffbab69fe4ed65f3c407426d044a1b0fc40a
ebudem <e.demirhan99@hotmail.com>
2025-03-13 23:53:52 +0100

    Change structure

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-const el = document.getElementById("h5p-container")
-const options = {
-    frameJs: '/assets/h5p-standalone/frame.bundle.js',
-    frameCss: '/assets/h5p-standalone/styles/h5p.css'
-}
-
-new H5PStandalone.H5P(el,options);
